f0781671fd7f1be3d5723ada91d2d81073f72642
Naga Brahma Chary <nagachary018@gmail.com>
2025-10-13 00:39:07 -0700

    added more features

--- src/main/java/com/spring/rest/SampleFluxMono.java
+++ src/main/java/com/spring/rest/SampleFluxMono.java
@@ -11,15 +11,17 @@ import java.util.Random;
 import java.util.function.Function;
 
 public class SampleFluxMono {
-
     private static final Logger logger = LoggerFactory.getLogger(SampleFluxMono.class);
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SampleFluxMono sampleFlux = new SampleFluxMono();
+
         logger.info("Flux values are :");
         sampleFlux.fluxString().subscribe(logger::info);
+
         logger.info("Mono value is :");
         sampleFlux.monoString().subscribe(logger::info);
+
         logger.info("fluxString_flatMap values are :");
         sampleFlux.fluxString_flatMap(5).subscribe(logger::info);
 
@@ -38,6 +40,20 @@ public class SampleFluxMono {
         logger.info("fluxString_transform value is :");
         sampleFlux.fluxString_transform(5).subscribe(x -> logger.info(String.valueOf(x)));
 
+        logger.info("fluxString_transform_switchIfEmpty value is :");
+        sampleFlux.fluxString_transform_switchIfEmpty(6).subscribe(x -> logger.info(String.valueOf(x)));
+
+        logger.info("fluxString_concat value is :");
+        sampleFlux.fluxString_concat().log().subscribe(logger::info);
+
+        logger.info("fluxString_concatWith value is :");
+        sampleFlux.fluxString_concatWith().log().subscribe(logger::info);
+
+        logger.info("monoString_concatWith value is :");
+        sampleFlux.monoString_concatWith().log().subscribe(logger::info);
+
+        logger.info("fluxString_merge value is :");
+        sampleFlux.fluxString_merge().log().subscribe(logger::info);
 
 
     }
@@ -59,13 +75,27 @@ public class SampleFluxMono {
     public Flux<String> fluxString_transform(int strLength) {
         Function<Flux<String>, Flux<String>> fluxFunction = input -> input.map(String::toUpperCase)
                 .filter(x -> x.length() > strLength);
+
         return Flux.fromIterable(List.of("Name1", "Name2", "Name 3"))
                 .transform(fluxFunction)
                 .flatMap(this::splitString)
+                .defaultIfEmpty("DEFAULT")
                 .log();
     }
 
+    public Flux<String> fluxString_transform_switchIfEmpty(int strLength) {
+        Function<Flux<String>, Flux<String>> fluxFunction = input -> input.map(String::toUpperCase)
+                .filter(x -> x.length() > strLength)
+                .flatMap(this::splitString);
 
+        var defaultFlux = Flux.just("default")
+                .transform(fluxFunction);
+
+        return Flux.fromIterable(List.of("Name1", "Name2", "Name 3"))
+                .transform(fluxFunction)
+                .switchIfEmpty(defaultFlux)
+                .log();
+    }
 
     public Flux<String> splitString(String name) {
         var charArray = name.split("");
@@ -73,6 +103,9 @@ public class SampleFluxMono {
 
     }
 
+    /**
+     * flat map doesn't emit the response in the order
+     */
     public Flux<String> fluxString_flatMap_async(int strLength) {
         return Flux.fromIterable(List.of("Name1", "Name2", "Name 3"))
                 .map(String::toUpperCase)
@@ -81,6 +114,9 @@ public class SampleFluxMono {
                 .log();
     }
 
+    /**
+     * concat map emits the response in the order
+     */
     public Flux<String> fluxString_concatMap_async(int strLength) {
         return Flux.fromIterable(List.of("Name1", "Name2", "Name 3"))
                 .map(String::toUpperCase)
@@ -127,4 +163,110 @@ public class SampleFluxMono {
                 .flatMapMany(this::splitString)
                 .log();
     }
+
+    /**
+     * concat is a static function in Flux
+     * concatWith is an instance method in Flux and Mono
+     *
+     */
+    public Flux<String> fluxString_concat() {
+        var firstFlux = Flux.just("Name 1", "Name 2");
+        var secondFlux = Flux.just("Name 3", "Name 4");
+
+        return Flux.concat(firstFlux, secondFlux).log();
+    }
+
+    public Flux<String> fluxString_concatWith() {
+        var firstFlux = Flux.just("Name 1", "Name 2");
+        var secondFlux = Flux.just("Name 3", "Name 4");
+
+        return firstFlux.concatWith(secondFlux).log();
+    }
+
+    public Flux<String> monoString_concatWith() {
+        var firstMono = Mono.just("Name M");
+        var secondMono = Flux.just("Name N");
+
+        return firstMono.concatWith(secondMono).log();
+    }
+
+    /**
+     * merge is a static function in Flux
+     * mergeWith is an instance method in Flux and Mono
+     *
+     */
+    public Flux<String> fluxString_merge() {
+        var firstFlux = Flux.just("Name 1", "Name 2")
+                .delayElements(Duration.ofMillis(100))
+                .log();
+        var secondFlux = Flux.just("Name 3", "Name 4")
+                .delayElements(Duration.ofMillis(110))
+                .log();
+
+       // return Flux.merge(firstFlux, secondFlux).log();
+        return firstFlux.mergeWith(secondFlux).log();
+    }
+
+    public Flux<String> monoString_mergeWith() {
+        var firstMono = Mono.just("Name M");
+        var secondMono = Flux.just("Name N");
+
+        return firstMono.mergeWith(secondMono).log();
+    }
+    /**
+     * fluxString_mergeSequential is a static function in Flux
+     *
+     */
+    public Flux<String> fluxString_mergeSequential() {
+        var firstFlux = Flux.just("Name 1", "Name 2")
+                .delayElements(Duration.ofMillis(100))
+                .log();
+        var secondFlux = Flux.just("Name 3", "Name 4")
+                .delayElements(Duration.ofMillis(110))
+                .log();
+
+      return Flux.mergeSequential(firstFlux, secondFlux).log();
+    }
+
+    /**
+     * zip is a static function in Flux
+     * zipWith is an instance method in Flux and Mono
+     */
+    public Flux<String> fluxString_zip() {
+        var firstFlux = Flux.just("Name 1", "Name 2");
+        var secondFlux = Flux.just("Name 3", "Name 4");
+
+        return Flux.zip(firstFlux, secondFlux, (firstELe, secondEle) -> firstELe + secondEle)
+                .log();
+    }
+
+    public Flux<String> fluxString_zip_1() {
+        var firstFlux = Flux.just("a", "b");
+        var secondFlux = Flux.just("c", "d");
+        var thirdFlux = Flux.just("1", "2");
+        var fourthFlux = Flux.just("3", "4");
+
+        return Flux.zip(firstFlux, secondFlux, thirdFlux, fourthFlux)
+                .map(t4 -> t4.getT1() + t4.getT2() + t4.getT3() + t4.getT4())
+                .log();
+    }
+
+    public Flux<String> fluxString_zipWith() {
+        var firstFlux = Flux.just("Name 1", "Name 2");
+        var secondFlux = Flux.just("Name 3", "Name 4");
+
+        return firstFlux.zipWith(secondFlux, (firstELe, secondEle) -> firstELe + secondEle)
+                .log();
+    }
+
+    public Mono<String> monoString_zipWith() {
+        var firstFlux = Mono.just("Name 1");
+        var secondFlux = Mono.just("Name 2");
+
+        return firstFlux.zipWith(secondFlux)
+                .map(t2 -> t2.getT1() + t2.getT2() )
+                .log();
+    }
+
+
 }
--- src/test/java/com/spring/rest/SampleFluxMonoTest.java
+++ src/test/java/com/spring/rest/SampleFluxMonoTest.java
@@ -10,35 +10,99 @@ public class SampleFluxMonoTest {
     @Test
     void fluxString() {
         var namesFlux = sampleFluxMono.fluxString();
-        StepVerifier.create(namesFlux).expectNextCount(3).verifyComplete();
-        StepVerifier.create(namesFlux).expectNext("NAME 1", "NAME 2", "NAME 3").verifyComplete();
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3).verifyComplete();
+        StepVerifier.create(namesFlux)
+                .expectNext("NAME 1", "NAME 2", "NAME 3").verifyComplete();
     }
 
     @Test
     void monoString() {
 
         var namesMono = sampleFluxMono.monoString();
-        StepVerifier.create(namesMono).expectNext("Name N").verifyComplete();
+        StepVerifier.create(namesMono)
+                .expectNext("NAME N").verifyComplete();
     }
 
     @Test
     void fluxStringFlatMap() {
         var fluxString_flatMap = sampleFluxMono.fluxString_flatMap(5);
-        StepVerifier.create(fluxString_flatMap).expectNext("N", "A", "M", "E", "", "3");
+        StepVerifier.create(fluxString_flatMap)
+                .expectNext("N", "A", "M", "E", "", "3");
 
     }
 
     @Test
     void fluxStringFlatMapAsync() {
         var fluxString_flatMap_async = sampleFluxMono.fluxString_flatMap_async(5);
-        StepVerifier.create(fluxString_flatMap_async).expectNextCount(6).verifyComplete();
+        StepVerifier.create(fluxString_flatMap_async)
+                .expectNextCount(6).verifyComplete();
 
     }
 
     @Test
     void fluxStringConcatMapAsync() {
         var fluxString_concatMap_async = sampleFluxMono.fluxString_concatMap_async(5);
-        StepVerifier.create(fluxString_concatMap_async).expectNext("N", "A", "M", "E", "", "3").verifyComplete();
+        StepVerifier.create(fluxString_concatMap_async)
+                .expectNextCount(6).verifyComplete();
 
     }
+
+    @Test
+    void fluxStringTransform() {
+        var fluxString_transform = sampleFluxMono.fluxString_transform(6);
+        StepVerifier.create(fluxString_transform)
+                .expectNext("DEFAULT").verifyComplete();
+    }
+
+    @Test
+    void fluxStringTransformSwitchIfEmpty() {
+        var fluxString_transform_switchIfEmpty = sampleFluxMono.fluxString_transform_switchIfEmpty(6);
+        StepVerifier.create(fluxString_transform_switchIfEmpty)
+                .expectNext("D", "E", "F", "A", "U", "L", "T").verifyComplete();
+    }
+
+    @Test
+    void fluxStringMerge() {
+       var string_merge =  sampleFluxMono.fluxString_merge();
+        StepVerifier.create(string_merge).expectNext("Name 1", "Name 3","Name 2", "Name 4").verifyComplete();
+    }
+
+    @Test
+    void monoStringMergeWith() {
+        var string_merge =  sampleFluxMono.monoString_mergeWith();
+        StepVerifier.create(string_merge).expectNext("Name M", "Name N").verifyComplete();
+    }
+
+    @Test
+    void fluxStringMergeSequential() {
+      var fluxString_mergeSequential = sampleFluxMono.fluxString_mergeSequential();
+      StepVerifier.create(fluxString_mergeSequential).expectNext("Name 1", "Name 2", "Name 3", "Name 4").verifyComplete();
+    }
+
+    @Test
+    void fluxStringZip() {
+        var fluxString_zip = sampleFluxMono.fluxString_zip();
+        StepVerifier.create(fluxString_zip).expectNext("Name 1Name 3", "Name 2Name 4").verifyComplete();
+    }
+
+    @Test
+    void fluxStringZipWith() {
+        var fluxString_zipWith = sampleFluxMono.fluxString_zipWith();
+        StepVerifier.create(fluxString_zipWith).expectNext("Name 1Name 3", "Name 2Name 4").verifyComplete();
+    }
+
+    @Test
+    void fluxStringZip1() {
+        var fluxString_zip_1 = sampleFluxMono.fluxString_zip_1();
+        StepVerifier.create(fluxString_zip_1).expectNext("ac13", "bd24").verifyComplete();
+    }
+
+    @Test
+    void monoStringZipWith() {
+        var monoString_zipWith = sampleFluxMono.monoString_zipWith();
+        StepVerifier.create(monoString_zipWith).expectNext("Name 1Name 2").verifyComplete();
+    }
+
+
 }
